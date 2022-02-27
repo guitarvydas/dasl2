1ff202a4fc84e60cec516139333db96a952a56c2
Paul Tarvydas <guitarvydas@gmail.com>
2022-02-27 06:44:47 -0500

    syn

--- lisp/macros.lisp
+++ lisp/macros.lisp
@@ -1,3 +1,7 @@
 (defmacro foreach (v kIn vlist kDo expr)
   (declare (ignore kIn kDo))
   `(mapc #'(lambda (,v) ,expr) ,vlist))
+
+(defmacro syn (name v &body body)
+  `(let ((,name ,v))
+     ,@body))
