d3a738dd74f771992a657737889dc4e571162298
Paul Tarvydas <guitarvydas@gmail.com>
2022-03-16 12:17:33 -0400

    linecounter

--- asca/molecularize.bash
+++ asca/molecularize.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
 prep=../../dscript/tools/prep
 cdir=`pwd`
-${prep} '.' '$' asct.ohm asct.glue --stop=1
+${prep} '.' '$' asct.ohm asct.glue --stop=1 --support=${cdir}/support.js
 
--- asca/support.js
+++ asca/support.js
@@ -1,3 +1,18 @@
 exports.encode = function (s) {
     return encodeURIComponent (s);
 }
+
+let linenumber = 1;
+exports.resetlinenumber = function () { linenumber = 1; }
+
+exports.inclinenumber = function (str) {
+    let n = (str.match (/%0A/g) || []).length;
+    console.error (str);
+    console.error (n);
+    linenumber += n;
+    return "";
+}
+
+exports.getlinenumber = function () {
+    return linenumber.toString ();
+}
