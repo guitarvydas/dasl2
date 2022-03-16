a8c3f14f36653ab6182533482e3f67525bc7a7ac
Paul Tarvydas <guitarvydas@gmail.com>
2022-03-16 08:07:00 -0400

    external code

--- asca/tokenize.bash
+++ asca/tokenize.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 prep=../../dscript/tools/prep
 cdir=`pwd`
-#${prep} '.' '$' ascatokenize.ohm identity-ascatokenize.glue --stop=1
+${prep} '.' '$' ascatokenize.ohm identity-ascatokenize.glue --stop=1
 ${prep} '.' '$' ascatokenize.ohm ascatokenize.glue --stop=1 --support=${cdir}/support.js
 
