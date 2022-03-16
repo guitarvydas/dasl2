ba7bec3e13a228b8e7a6b76115d35585e7923160
Paul Tarvydas <guitarvydas@gmail.com>
2022-03-16 06:34:54 -0400

    lexical tokens

--- asca/tokenize.bash
+++ asca/tokenize.bash
@@ -1,4 +1,6 @@
 #!/bin/bash
 prep=../../dscript/tools/prep
-${prep} '.' '$' ascatokenize.ohm identity-ascatokenize.glue --stop=1
+cdir=`pwd`
+#${prep} '.' '$' ascatokenize.ohm identity-ascatokenize.glue --stop=1
+${prep} '.' '$' ascatokenize.ohm ascatokenize.glue --stop=1 --support=${cdir}/support.js
 
