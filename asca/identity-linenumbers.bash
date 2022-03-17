708648dbd1f58839fde71fde2f36d1fefbf6bfcd
Paul Tarvydas <guitarvydas@gmail.com>
2022-03-17 08:23:58 -0400

    basic tokenize various tests

---
+++ asca/identity-linenumbers.bash
@@ -0,0 +1,5 @@
+#!/bin/bash
+prep=../../dscript/tools/prep
+cdir=`pwd`
+${prep} '.' '$' linenumbers.ohm identity-linenumbers.glue --stop=1
+
---
+++ asca/linenumbers.bash
@@ -0,0 +1,5 @@
+#!/bin/bash
+prep=../../dscript/tools/prep
+cdir=`pwd`
+${prep} '.' '$' linenumbers.ohm linenumbers.glue --stop=1
+
