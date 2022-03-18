2270442c8361d3f7f12210174d25b0b9287c80f5
Paul Tarvydas <guitarvydas@gmail.com>
2022-03-18 18:10:00 -0400

    linenumbers grammar inherits from basictoken

--- asca/identity-linenumbers.bash
+++ asca/identity-linenumbers.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
 prep=~/tools/pre/pre
 cdir=`pwd`
-${prep} '.' '$' linenumbers.ohm identity-linenumbers.glue --stop=1
+${prep} '.' '$' linenumbers.ohm identity-linenumbers.glue --stop=1 --grammarname=linenumbers
 
--- asca/linenumbers.bash
+++ asca/linenumbers.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
 prep=~/tools/pre/pre
 cdir=`pwd`
-${prep} '.' '$' linenumbers.ohm linenumbers.glue --stop=1 --support=${cdir}/support.js
+${prep} '.' '$' linenumbers.ohm linenumbers.glue --stop=1 --support=${cdir}/support.js --grammarname=linenumbers
 
