87e608de82e6e2c6888fc8539874f624447f359b
Paul Tarvydas <guitarvydas@gmail.com>
2022-03-19 16:22:17 -0400

    tightened up tr.py ; make tr->lookup

--- asca/tr.py
+++ asca/tr.py
@@ -7,74 +7,74 @@ def printkw (tag, line):
     print ('{"token":"keyword","tag":"' + tag + '"}, ', end="")
     print (line,end="")
 for line in sys.stdin:
-    if (re.search (r'"content":"Yes"',line)):
+    if (re.search (r'"token":"ident".+"content":"Yes"',line)):
         printkw ("yes", line)
-    elif (re.search (r'"content":"No"',line)):
+    elif (re.search (r'"token":"ident".+"content":"No"',line)):
         printkw ("no", line)
 
-    elif (re.search (r'"content":"Nil"',line)):
+    elif (re.search (r'"token":"ident".+"content":"Nil"',line)):
         printkw ("nil", line)
-    elif (re.search (r'"content":"Trigger"',line)):
+    elif (re.search (r'"token":"ident".+"content":"Trigger"',line)):
         printkw ("trigger", line)
-    elif (re.search (r'"content":"Conclude"',line)):
+    elif (re.search (r'"token":"ident".+"content":"Conclude"',line)):
         printkw ("conclude", line)
-    elif (re.search (r'"content":"Args"',line)):
+    elif (re.search (r'"token":"ident".+"content":"Args"',line)):
         printkw ("args", line)
-    elif (re.search (r'"content":"Return"',line)):
+    elif (re.search (r'"token":"ident".+"content":"Return"',line)):
         printkw ("return", line)
-    elif (re.search (r'"content":"Send"',line)):
+    elif (re.search (r'"token":"ident".+"content":"Send"',line)):
         printkw ("send", line)
-    elif (re.search (r'"content":"Inject"',line)):
+    elif (re.search (r'"token":"ident".+"content":"Inject"',line)):
         printkw ("inject", line)
-    elif (re.search (r'"content":"%3Fdata"',line)):
+    elif (re.search (r'"token":"ident".+"content":"%3Fdata"',line)):
         printkw ("messagedata", line)
-    elif (re.search (r'"content":"%3Fetag"',line)):
+    elif (re.search (r'"token":"ident".+"content":"%3Fetag"',line)):
         printkw ("messageetag", line)
-    elif (re.search (r'"content":"connections"',line)):
+    elif (re.search (r'"token":"ident".+"content":"connections"',line)):
         printkw ("connections", line)
 
-    elif (re.search (r'"content":"%E2%88%9E"',line)):
+    elif (re.search (r'"token":"lex".+"content":"%E2%88%9E"',line)):
         printkw ("lookup", line)
-    elif (re.search (r'"content":"%CE%BB"',line)):
+    elif (re.search (r'"token":"lex".+"content":"%CE%BB"',line)):
         printkw ("lambda", line)
-    elif (re.search (r'"content":"."',line)):
+    elif (re.search (r'"token":"lex".+"content":"."',line)):
         printkw ("dot", line)
-    elif (re.search (r'"content":"\("',line)):
+    elif (re.search (r'"token":"lex".+"content":"\("',line)):
         printkw ("lpar", line)
-    elif (re.search (r'"content":"\)"',line)):
+    elif (re.search (r'"token":"lex".+"content":"\)"',line)):
         printkw ("rpar", line)
-    elif (re.search (r'"content":"%5B"',line)):
+    elif (re.search (r'"token":"lex".+"content":"%5B"',line)):
         printkw ("lbracket", line)
-    elif (re.search (r'"content":"%5D"',line)):
+    elif (re.search (r'"token":"lex".+"content":"%5D"',line)):
         printkw ("rbracket", line)
-    elif (re.search (r'"content":"%C2%AB"',line)):
+    elif (re.search (r'"token":"lex".+"content":"%C2%AB"',line)):
         printkw ("lport", line)
-    elif (re.search (r'"content":"%C2%BB"',line)):
+    elif (re.search (r'"token":"lex".+"content":"%C2%BB"',line)):
         printkw ("rport", line)
 
-    elif (re.search (r'"content":"def"',line)):
+    elif (re.search (r'"token":"ident".+"content":"def"',line)):
         printkw ("def", line)
-    elif (re.search (r'"content":"etags"',line)):
+    elif (re.search (r'"token":"ident".+"content":"etags"',line)):
         printkw ("etags", line)
-    elif (re.search (r'"content":"name"',line)):
+    elif (re.search (r'"token":"ident".+"content":"name"',line)):
         printkw ("name", line)
-    elif (re.search (r'"content":"signature"',line)):
+    elif (re.search (r'"token":"ident".+"content":"signature"',line)):
         printkw ("signature", line)
-    elif (re.search (r'"content":"inputs"',line)):
+    elif (re.search (r'"token":"ident".+"content":"inputs"',line)):
         printkw ("inputs", line)
-    elif (re.search (r'"content":"outputs"',line)):
+    elif (re.search (r'"token":"ident".+"content":"outputs"',line)):
         printkw ("outputs", line)
-    elif (re.search (r'"content":"nets"',line)):
+    elif (re.search (r'"token":"ident".+"content":"nets"',line)):
         printkw ("nets", line)
-    elif (re.search (r'"content":"locals"',line)):
+    elif (re.search (r'"token":"ident".+"content":"locals"',line)):
         printkw ("locals", line)
-    elif (re.search (r'"content":"initially"',line)):
+    elif (re.search (r'"token":"ident".+"content":"initially"',line)):
         printkw ("initially", line)
-    elif (re.search (r'"content":"handler"',line)):
+    elif (re.search (r'"token":"ident".+"content":"handler"',line)):
         printkw ("handler", line)
-    elif (re.search (r'"content":"finally"',line)):
+    elif (re.search (r'"token":"ident".+"content":"finally"',line)):
         printkw ("finally", line)
-    elif (re.search (r'"content":"children"',line)):
+    elif (re.search (r'"token":"ident".+"content":"children"',line)):
         printkw ("children", line)
 
     else:
