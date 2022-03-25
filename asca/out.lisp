bf9f26adfac2d7450094664e5cb91fc7062a9369
Paul Tarvydas <guitarvydas@gmail.com>
2022-03-25 08:44:27 -0400

    all ops beta

--- asca/out.lisp
+++ asca/out.lisp
@@ -39,9 +39,109 @@
 (("unsuccessful" . "found") . (("self" . "found")))
 (("successful" . "found") . (("self" . "found")))
 (("successful" . "answer") . (("self" . "answer")))))))
+,(defparameter *scroll-through-atoms*
+`(
+(name . "scroll through atoms")
+(etags . ("name" "advance" "EOF" "try 1 name match" ))
+($args . nil)
+(inputs . ("name" "advance" ))
+(outputs . ("EOF" "try 1 name match" ))
+(nets . ())
+(locals . ())
+(initially . 
+,(lambda ($context) 
+(let ((atom-memory ($?field ($?field $context '$args) 'atom-memory)))
+
+(cond (("eof?" "atom-memory") |Yes:
+($send '("scroll through atoms" "EOF") No)|No:
+nil)))))
+(handler . 
+,(lambda ($context $message) 
+(let ((atom-memory ($?field ($?field $context '$args) 'atom-memory)))
+)⎡•"name":
+($send '("scroll through atoms" "try 1 name match") Trigger)
+($dispatch-conclude $context)•"advance":
+("advanceToNextAtom" "atom-memory")
+(cond (("eof?" "atom-memory") |Yes:
+($send '("scroll through atoms" "EOF") No)|No:
+($send '("scroll through atoms" "try 1 name match") Trigger)))⎦))
+(finally . nil)
+(children . nil)
+(connections . nil)))
+,(defparameter *match-single-atom-name*
+`(
+(name . "match single atom name")
+(etags . ("name" "advance" "EOF" "try 1 name match" ))
+($args . nil)
+(inputs . ("name" "advance" ))
+(outputs . ("EOF" "try 1 name match" ))
+(nets . ())
+(locals . ())
+(initially . nil)
+(handler . 
+,(lambda ($context $message) ⎡•"go":
+(cond ((""match string "") |Yes:
+($send '("match single atom name" "ok") Trigger)|No:
+($send '("match single atom name" "mismatch") Trigger)))⎦))
+(finally . nil)
+(children . nil)
+(connections . nil)))
+,(defparameter *unsuccessful*
+`(
+(name . "unsuccessful")
+(etags . ("conclude" "found" ))
+($args . nil)
+(inputs . ("conclude" ))
+(outputs . ("found" ))
+(nets . ())
+(locals . ())
+(initially . nil)
+(handler . 
+,(lambda ($context $message) ⎡•"conclude":
+($send '("unsuccessful" "found") No)⎦))
+(finally . nil)
+(children . nil)
+(connections . nil)))
+,(defparameter *successful*
+`(
+(name . "successful")
+(etags . ("conclude" "found" "answer" ))
+($args . nil)
+(inputs . ("conclude" ))
+(outputs . ("found" "answer" ))
+(nets . ())
+(locals . ())
+(initially . nil)
+(handler . 
+,(lambda ($context $message) ⎡•"conclude":
+($send '("successful" "answer") (?edata-from-message ?data))
+($send '("successful" "found") Yes)⎦))
+(finally . nil)
+(children . nil)
+(connections . nil)))
 
 (defparameter *lookup-signature*
 (name . "lookup")
 (etags . ("name" "found" "answer" ))
 (inputs . ("name" ))
+(outputs . ("found" "answer" ))),
+(defparameter *scroll-through-atoms-signature*
+(name . "scroll through atoms")
+(etags . ("name" "advance" "EOF" "try 1 name match" ))
+(inputs . ("name" "advance" ))
+(outputs . ("EOF" "try 1 name match" ))),
+(defparameter *match-single-atom-name-signature*
+(name . "match single atom name")
+(etags . ("name" "advance" "EOF" "try 1 name match" ))
+(inputs . ("name" "advance" ))
+(outputs . ("EOF" "try 1 name match" ))),
+(defparameter *unsuccessful-signature*
+(name . "unsuccessful")
+(etags . ("conclude" "found" ))
+(inputs . ("conclude" ))
+(outputs . ("found" ))),
+(defparameter *successful-signature*
+(name . "successful")
+(etags . ("conclude" "found" "answer" ))
+(inputs . ("conclude" ))
 (outputs . ("found" "answer" )))
